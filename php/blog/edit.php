eeedc1e3967cdf6f270bc81008f4dc33e3a9aff7
sakshimjain <3135sakshi@gmail.com>
2018-05-22 12:38:12 +0530

    Update edit.php

--- php/blog/edit.php
+++ php/blog/edit.php
@@ -13,6 +13,11 @@ $database = "BLOG";
 // Create connection
 $conn = new mysqli($servername, $username, $password , $database);
 $con=$fnameErr=$id="";
+if(empty($_SESSION["email"]))
+{
+    echo "Access denied";
+    exit;
+}
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
    
@@ -64,8 +69,8 @@ function test_input($data)
 ?>
 
 <html>
-<style>
-    .Large
+<style><br><br>
+    .La    <a href = "logout.php">Logout</a>
 {
     size="35";
     height: 150px;
@@ -88,13 +93,16 @@ function test_input($data)
           //     $content=$row["content"];
     ?>
 <h1> Write your heart out!!</h1>
-    Content: <input type="text"  class="Large" name="con" value ="<?php echo $content ?>" align="middle">
+
+    Content: <textarea name="con" rows="20" cols="80" ><?php echo $content ;?></textarea>
+    <!--Content: <input type="text"  class="Large" name="con" value ="<?php// echo $content ?>" align="middle">-->
    
     
     <br><br>
-    Id:<input type="numeric"  name="ide" value ="<?php echo $id ?>" align="middle">
+    <input type="hidden"  name="ide" value ="<?php echo $id ?>" align="middle">
     <input type="submit" name="Submits" value="Update"><br><br><br>
-   
+    <br><br>
+    <a href = "logout.php">Logout</a>
 
 
 
