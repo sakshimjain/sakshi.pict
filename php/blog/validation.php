3655f6d4a23b04b176fc979f37a33c8578e5ec40
sakshimjain <3135sakshi@gmail.com>
2018-05-23 19:03:52 +0530

    Update validation.php

--- php/blog/validation.php
+++ php/blog/validation.php
@@ -15,26 +15,45 @@ if(isset($_REQUEST['submit']))
     $_SESSION["email"]="$email";
     $_SESSION["password"]="$password";
     $_SESSION["content"]="";
-    $_SESSION["role_id"]="$role_id";
-    $sql =  "SELECT email,password,role_id FROM USER1 WHERE email = '$email' and password = SHA1('$password')";
+    
+   
+    $sql =  "SELECT email,password,role_id,id FROM USER1 WHERE email = '$email' and password = SHA1('$password')";
    // $sql = "SELECT password FROM USER1"
 
     $result = mysqli_query($conn, $sql);                            //The mysqli_query() function performs a query against the database
+   
     // $result = $conn->query("SELECT name FROM Employee WHERE name = '$name'");
     if (!$row = mysqli_fetch_assoc($result)) {                          //The mysqli_fetch_assoc() function fetches a result row as an associative array.  printf ("%s (%s)\n",$row["Lastname"],$row["Age"]);
         echo "Your username or password  is incorrect!";
+        $_SESSION["role_id"]=$row["role_id"];
+        $_SESSION["id"]=$row["id"];
+       
     }
     else
     {
+        $_SESSION["id"]=$row["id"];
+       
+        $_SESSION["role_id"]=$row["role_id"];
+       
           if( $row["role_id"] == '3')
           {
               header("Location: editor.php");
               exit;
           }
-          else{echo "Login Successful";
-          header("Location: main.php");
-          exit;}
+          if($row["role_id"] == '1')
+          {
+              echo "Login Successful";
+              header("Location: index.php");
+              exit;
+          }
+          if($row["role_id"] == '2')
+          {
+              echo "Login Successful";
+              header("Location: editor.php");
+              exit;
+          }
     }
+  
   
 
 }
